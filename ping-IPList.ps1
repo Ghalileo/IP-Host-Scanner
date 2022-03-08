18f5f00f69b2b4244b4bfda34f4cee7b0e9c97b7
Ghalileo <ose.okogbo@gmail.com>
2022-03-08 16:56:44 -0600

    now able to print response to text file based on ip status

--- ping-IPList.ps1
+++ ping-IPList.ps1
@@ -11,9 +11,13 @@ Write-Host "Pinging Started...."
 foreach($ip in $theIPAddresses) {
     if (test-connection $ip.("Address") -count 1 -quiet){
         write-host $ip.("Address") "Successful Ping to Host." -foreground Magenta
+        
+        echo $ip. >> online.txt
 
     } else {
         write-host $ip.("Address") "Failed to Ping Host." -foreground DarkRed 
+        
+        echo $ip. >> offline.txt
     }
 }
 
