ac97b8d73c5ff8dce0df9f1fd4b03522f81cc33e
Ghalileo <ose.okogbo@gmail.com>
2022-03-09 14:26:06 -0600

    working as inteded.  lists render in txt

--- ping-IPList.ps1
+++ ping-IPList.ps1
@@ -7,17 +7,19 @@ $IPColumn = "Address"
 Write-Host "Reading File" $csvfile
 $theIPAddresses = import-csv $csvfile | select-object $IPColumn
 
+
 Write-Host "Pinging Started...."
 foreach($ip in $theIPAddresses) {
     if (test-connection $ip.("Address") -count 1 -quiet){
         write-host $ip.("Address") "Successful Ping to Host." -foreground Magenta
-        
-        echo $ip. >> online.txt
+     
+        echo $ip.("Address") >> online.txt
+        # echo $res. >> online.txt
 
     } else {
         write-host $ip.("Address") "Failed to Ping Host." -foreground DarkRed 
         
-        echo $ip. >> offline.txt
+        echo $ip.("Address") >> offline.txt
     }
 }
 
